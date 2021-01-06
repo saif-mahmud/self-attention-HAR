c059843b857acd2b64b34d6ff3bff218e6a90261
Saif Mahmud <saif.dhrubo@gmail.com>
2021-01-06 22:10:48 +0600

    data: Update routine for sliding window segmentation

--- preprocess/sliding_window.py
+++ preprocess/sliding_window.py
@@ -1,8 +1,6 @@
-import tensorflow as tf
 import numpy as np
-import pandas as pd
-import seaborn as sn
-import matplotlib.pyplot as plt
+from scipy import stats
+
 
 def windowz(data, size, use_overlap=True):
     start = 0
@@ -14,50 +12,50 @@ def windowz(data, size, use_overlap=True):
             start += size
 
 
-def segment_pa2_test(x_test,y_test,window_size,n_sensor_val):
-    segments = np.zeros(((len(x_test)//(window_size))+1,window_size,n_sensor_val))
-    labels = np.zeros(((len(y_test)//(window_size))+1))
+def segment_pa2_test(x_test, y_test, window_size, n_sensor_val):
+    segments = np.zeros(((len(x_test) // (window_size)) + 1, window_size, n_sensor_val))
+    labels = np.zeros(((len(y_test) // (window_size)) + 1))
     i_segment = 0
     i_label = 0
-    for (start,end) in windowz(x_test,window_size,use_overlap=False):
+    for (start, end) in windowz(x_test, window_size, use_overlap=False):
         if end >= x_test.shape[0]:
             pad_len = window_size - len(x_test[start:end])
-            segments[i_segment] = x_test[start-pad_len:end]
-            m = stats.mode(y_test[start-pad_len:end])
+            segments[i_segment] = x_test[start - pad_len:end]
+            m = stats.mode(y_test[start - pad_len:end])
             labels[i_label] = m[0]
         else:
             m = stats.mode(y_test[start:end])
             segments[i_segment] = x_test[start:end]
             labels[i_label] = m[0]
-            i_label+=1
-            i_segment+=1
+            i_label += 1
+            i_segment += 1
 
     return segments, labels
 
 
-def segment_pa2(x_train,y_train,window_size,n_sensor_val):
-    segments = np.zeros(((len(x_train)//(window_size//2))-1,window_size,n_sensor_val))
-    labels = np.zeros(((len(y_train)//(window_size//2))-1))
+def segment_pa2(x_train, y_train, window_size, n_sensor_val):
+    segments = np.zeros(((len(x_train) // (window_size // 2)) - 1, window_size, n_sensor_val))
+    labels = np.zeros(((len(y_train) // (window_size // 2)) - 1))
     i_segment = 0
     i_label = 0
-    for (start,end) in windowz(x_train,window_size):
-        if(len(x_train[start:end]) == window_size):
+    for (start, end) in windowz(x_train, window_size):
+        if (len(x_train[start:end]) == window_size):
             m = stats.mode(y_train[start:end])
             segments[i_segment] = x_train[start:end]
             labels[i_label] = m[0]
-            i_label+=1
-            i_segment+=1
+            i_label += 1
+            i_segment += 1
     return segments, labels
 
 
-def unsegment_pa2_test(y_preds,total_length,window_size):
+def unsegment_pa2_test(y_preds, total_length, window_size):
     unsegmented_preds = np.zeros((total_length,))
     start = 0
     end = window_size
     for element in y_preds:
         if end >= total_length:
             end = total_length
-        for i in range(start,end):
+        for i in range(start, end):
             unsegmented_preds[i] = element
         start = end
         end += window_size
